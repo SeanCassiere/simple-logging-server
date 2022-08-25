cd1f5dd0b92290554cee3b740ebe75351726c95c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-08-25 14:53:32 +0530

    fix: setting the fastify server host on init

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,26 @@ import { loggingRoutes } from "./components/logging/logging.routes";
 import { env } from "./config/env";
 const packageJson = require("../package.json");
 
-const app = fastify({
-  maxParamLength: 5000,
-});
+const app = fastify({ logger: env.NODE_ENV !== "production" });
 
 app.register(loggingRoutes, { prefix: "/api/logs" });
 
+app.get("/", (_, reply) => {
+  reply.code(200).send({ message: "Hello World" });
+});
+
 async function main() {
   try {
-    await app.listen({ port: Number(env.PORT) });
+    app.listen(
+      { port: Number(env.PORT), host: env.NODE_ENV !== "production" ? "127.0.0.1" : "0.0.0.0" },
+      (err, address) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        console.log(`${packageJson.name} (${env.NODE_ENV} - ${packageJson.version}) listening on ${address} `);
+      }
+    );
     console.log(
       `${packageJson.name} (${env.NODE_ENV} - ${packageJson.version}) is running and listening on port ${env.PORT} 🚀`
     );
