2b4239e90d9488c9328ef003a2fb2c080fe08c57
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-08-29 23:29:06 +0530

    feat: uiConfig for swagger and rate-limit removed in dev

--- src/index.ts
+++ src/index.ts
@@ -14,48 +14,51 @@ const packageJson = require("../package.json");
 
 const app = fastify({
   logger: env.NODE_ENV !== "production",
-  // logger: false,
-});
-
-app.register(rateLimit, {
-  max: 50,
-  timeWindow: "1 minute",
-  // ...(env.NODE_ENV !== "production" ? { allowList: ["127.0.0.1", "localhost"] } : {}),
-});
-
-app.get("/health", async (_, reply) => {
-  reply.code(200).send({
-    status: "ok",
-    uptime: process.uptime() ?? 0,
-  });
-});
-
-for (const schema of [...logSchemas, ...serviceSchemas]) {
-  app.addSchema(schema);
-}
-
-app.register(
-  swagger,
-  withRefResolver({
-    routePrefix: "/docs",
-    exposeRoute: true,
-    staticCSP: true,
-    openapi: {
-      info: {
-        title: "Logging API",
-        version: packageJson.version,
-      },
-    },
-  })
-);
-
-app.register(serviceRoutes, { prefix: "/api/services" });
-
-app.get("/", (_, reply) => {
-  reply.code(200).send({ message: "Hello World" });
 });
 
 async function main() {
+  app.register(rateLimit, {
+    max: 50,
+    timeWindow: "1 minute",
+    ...(env.NODE_ENV !== "production" ? { allowList: ["127.0.0.1", "localhost"] } : {}),
+  });
+
+  app.get("/health", async (_, reply) => {
+    reply.code(200).send({
+      status: "ok",
+      uptime: process.uptime() ?? 0,
+    });
+  });
+
+  for (const schema of [...logSchemas, ...serviceSchemas]) {
+    app.addSchema(schema);
+  }
+
+  app.register(
+    swagger,
+    withRefResolver({
+      routePrefix: "/docs",
+      exposeRoute: true,
+      staticCSP: true,
+      openapi: {
+        info: {
+          title: "Logging API",
+          version: packageJson.version,
+        },
+      },
+      uiConfig: {
+        docExpansion: "list",
+        displayRequestDuration: true,
+      },
+    })
+  );
+
+  app.register(serviceRoutes, { prefix: "/api/services" });
+
+  app.get("/", (_, reply) => {
+    reply.code(200).send({ message: "Hello World" });
+  });
+
   try {
     app.listen(
       { port: Number(env.PORT), host: env.NODE_ENV !== "production" ? "127.0.0.1" : "0.0.0.0" },
