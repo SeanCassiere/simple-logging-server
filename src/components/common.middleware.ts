bf690c4e1ca20516dc168129787e6cd23f3aa545
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-24 22:55:24 +1200

    fix: bug causing request to be fired twice

--- src/components/common.middleware.ts
+++ src/components/common.middleware.ts
@@ -1,16 +1,16 @@
-import type { FastifyReply, FastifyRequest, DoneFuncWithErrOrRes } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 
 import { findActiveService } from "./services/services.service";
 
 export function serviceIdMiddleware(opts?: { checkAdmin?: boolean }) {
   const { checkAdmin = false } = opts ?? {};
 
-  return async function (request: FastifyRequest<{ Headers: any }>, reply: FastifyReply, done: DoneFuncWithErrOrRes) {
+  return async function (request: FastifyRequest<{ Headers: any }>, reply: FastifyReply) {
     const xAppServiceId = request.headers["x-app-service-id"];
 
     if (!xAppServiceId || typeof xAppServiceId !== "string") {
       reply.code(401).send({ success: false, message: `X-APP-SERVICE-ID header was not passed.` });
-      return done(new Error("X-APP-SERVICE-ID header was not passed."));
+      return;
     }
 
     const client = await findActiveService({ serviceId: xAppServiceId, ...(checkAdmin ? { isAdmin: true } : {}) });
@@ -20,9 +20,9 @@ export function serviceIdMiddleware(opts?: { checkAdmin?: boolean }) {
         success: false,
         message: `${xAppServiceId} - service does not exist or does not have necessary rights`,
       });
-      return done(new Error("Client does not exist or does not have necessary rights"));
+      return;
     }
 
-    return done();
+    return;
   };
 }
