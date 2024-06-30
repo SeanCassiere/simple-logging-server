cce235c521893022471b33d95d8b1b47e92617e9
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-30 15:36:05 +1200

    chore: meta tags

--- src/routers/app/ui/layouts/root-document.tsx
+++ src/routers/app/ui/layouts/root-document.tsx
@@ -4,6 +4,9 @@ export const RootDocument: FC<PropsWithChildren<{ title: string }>> = ({ title, 
   return (
     <html>
       <head>
+        <meta charset="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <title>{title}</title>
         <script
           src="https://unpkg.com/htmx.org@1.9.10"
           integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
@@ -11,7 +14,6 @@ export const RootDocument: FC<PropsWithChildren<{ title: string }>> = ({ title, 
         ></script>
         <script src="https://unpkg.com/htmx.org/dist/ext/json-enc.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
-        <title>{title}</title>
       </head>
       <body>{children}</body>
     </html>
