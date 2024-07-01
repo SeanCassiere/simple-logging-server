58e4a95b94fa3ec9cb0358f02711fd1252316e7e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-02 07:17:15 +1200

    refactor(ui): blank page content when no organizations are available

--- src/routers/app/ui/pages/app.index.tsx
+++ src/routers/app/ui/pages/app.index.tsx
@@ -2,12 +2,17 @@ import type { FC } from "hono/jsx";
 
 import { RootDocument } from "../layouts/root-document.js";
 import { AppContainer, type AppContainerProps } from "../layouts/app-container.js";
+import { Button } from "../components/button.js";
 
-export const DashboardLandingPage: FC<{} & Omit<AppContainerProps, "tenant">> = ({ user, tenants }) => {
+export const NoOrganizationPage: FC<{} & Omit<AppContainerProps, "tenant">> = ({ user, tenants }) => {
   return (
     <RootDocument title="Simple Logging Server">
       <AppContainer user={user} tenants={tenants} tenant={null} mainClass="p-2 md:p-4">
-        <p>There is no content on this page</p>
+        <div class="grid gap-2">
+          <p>You are not a part of any active organizations.</p>
+          <p>Create your own organization!.</p>
+          <Button class="w-auto">Create organization</Button>
+        </div>
       </AppContainer>
     </RootDocument>
   );
